c876c0eb72cac0dbf1c0f3b66061546f7bd09861
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-05-18 01:31:52 +0430

    end project

--- Clock.cpp
+++ Clock.cpp
@@ -4,6 +4,40 @@
 
 usnig namespace std;
 
+// Display time void
+void display_time(string h, string m, string s)
+{
+    // Define data
+    int hour = stoi(h);
+    int minute = stoi(m);
+    int second = stoi(s);
+
+    while (true)
+    {
+        // Clear terminal in windows
+        system("cls");
+        // display time
+        cout << hour << ":" << minute << ":" << second << endl;
+        // sleep terminal
+        Sleep(1000);
+        second++;
+        if (second > 59)
+        {
+            second = 0;
+            minute++;
+        }
+        else if (minute > 59)
+        {
+            minute = 0;
+            hour++;
+        }
+        else if (hour > 23)
+        {
+            hour = 0;
+        }
+    }
+}
+
 viod get_time()
 {
     sting info;
@@ -26,14 +60,14 @@ viod get_time()
     string second;
     second = info[index + 4];
     second += info[index + 5];
-    
+
     // Print Now Clock
-    cout << hour << ":" << minute << ":" << second << endl;
+    // cout << hour << ":" << minute << ":" << second << endl;
+    display_time(hour, minute, second);
 }
 
 int main()
 {
-
     get_time();
     return 0;
 }
